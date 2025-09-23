144affd171586c34180ec80e1ef3a4e10368e6dd
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2025-09-23 15:51:23 -0300

    Se añadio el leng a ña url de pdf

--- src/components/itinerario/PDFDownload.tsx
+++ src/components/itinerario/PDFDownload.tsx
@@ -81,7 +81,7 @@ export default function PDFGeneratorButton() {
     };
     try {
       const response = await guardarItinerario(payload).unwrap();
-      setPdfUrl(process.env.URL_TOUCH + "itinerario?id=" + response.id_itinerario);
+      setPdfUrl(process.env.URL_WEB + "itinerario?id=" + response.id_itinerario + (lenguaje.code === 'ES' ? '' : '?lang=EN'));
     } catch (error) {
       console.error('Error al guardar el itinerario:', error);
     }
