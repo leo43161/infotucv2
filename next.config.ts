4ea4253765d63c44e78bcbf411c2a6705fd6bb2f
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2025-12-30 12:27:39 -0300

    Se añadio el stream del cadillal al touch adaptandolo de manera inteligente

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
     URL_PDF: "https://www.tucumanturismo.gob.ar/api/itinerario/pdf/",
     URL_IMG: "https://www.tucumanturismo.gob.ar/public/img/",
     URL_WEB: "https://www.tucumanturismo.gob.ar/",
-    URL_TOUCH: process.env.NODE_ENV === 'production' ? "https://www.tucumanturismo.gob.ar/infotucdev/" : "http://10.20.20.5:3000/",
-    URL_IMG_TOUCH: process.env.NODE_ENV === 'production' ? "/infotucdev" : "",
+    URL_TOUCH: process.env.NODE_ENV === 'production' ? "https://www.tucumanturismo.gob.ar/infotuc/" : "http://10.20.20.5:3000/",
+    URL_IMG_TOUCH: process.env.NODE_ENV === 'production' ? "/infotuc" : "",
   },
   devIndicators: false,
   /* config options here */
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? "/infotucdev" : "",
+  basePath: process.env.NODE_ENV === 'production' ? "/infotuc" : "",
   eslint: {
     ignoreDuringBuilds: true,
   },
